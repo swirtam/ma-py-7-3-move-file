e364b94199f6f773acc3348fd20ebe1a67e65cc0
Maja Swirta <maja.w.swirta@gmail.com>
2026-04-24 22:04:55 +0200

    Solution with improved style

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from os import mkdir, path, remove
+from os import (mkdir, path, remove)
 
 
 def move_file(command: str) -> None:
@@ -9,15 +9,16 @@ def move_file(command: str) -> None:
     move_command, file_original, file_moved = command_parts
     if move_command != "mv" or not path.isfile(file_original):
         return
-    if file_moved.endswith("/"):
+    path_moved_split = path.split(file_moved)
+    if not path_moved_split[1]:
         file_moved = path.join(file_moved, path.basename(file_original))
     if path.isfile(file_moved):
         return
 
     if (path.dirname(path.abspath(file_original))
             != path.dirname(path.abspath(file_moved))):
-        path_parts = path.normpath(path.dirname(file_moved)).split(path.sep)
-        # path_parts.remove("")   # for Linux
+        path_parts = path.normpath(path_moved_split[0]).lstrip(
+            path.sep).split(path.sep)
         path_moved = ""
         for part in path_parts:
             path_moved = path.join(path_moved, part)
