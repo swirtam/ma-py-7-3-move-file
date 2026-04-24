a23dacdba4eb2d974458bfafff0f4c7c6edd7fa9
Maja Swirta <maja.w.swirta@gmail.com>
2026-04-24 21:39:22 +0200

    Solution with mkdir and remove functions

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from os import makedirs, path, rename
+from os import mkdir, path, remove
 
 
 def move_file(command: str) -> None:
@@ -9,13 +9,25 @@ def move_file(command: str) -> None:
     move_command, file_original, file_moved = command_parts
     if move_command != "mv" or not path.isfile(file_original):
         return
-    if not path.splitext(path.basename(file_moved))[1]:
+    if file_moved.endswith("/"):
         file_moved = path.join(file_moved, path.basename(file_original))
     if path.isfile(file_moved):
         return
 
-    file_original = path.abspath(file_original)
-    file_moved = path.abspath(file_moved)
-    if path.dirname(file_moved) != path.dirname(file_original):
-        makedirs(path.dirname(file_moved), exist_ok=True)
-    rename(file_original, file_moved)
+    if (path.dirname(path.abspath(file_original))
+            != path.dirname(path.abspath(file_moved))):
+        path_parts = path.normpath(path.dirname(file_moved)).split(path.sep)
+        # path_parts.remove("")   # for Linux
+        path_moved = ""
+        for part in path_parts:
+            path_moved = path.join(path_moved, part)
+            if not path.isdir(path_moved):
+                mkdir(path_moved)
+
+    with (
+        open(file_original, "r") as fobj_original,
+        open(file_moved, "w") as fobj_moved
+    ):
+        for line in fobj_original.readlines():
+            fobj_moved.write(line)
+    remove(file_original)
